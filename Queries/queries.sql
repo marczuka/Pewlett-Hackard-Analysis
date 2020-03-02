9c09968572dd9eb9893e82c80355ac7ff60b97ec
Anna Marczuk <anna.marczuk@gmail.com>
2020-03-01 20:55:38 -0800

    Adding queries generating additional list of retiring employees.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -67,9 +67,85 @@ SELECT * FROM current_emp;
 SELECT COUNT(ce.emp_no), de.dept_no
 INTO retire_emp_by_dept
 FROM current_emp AS ce
-LEFT JOIN dept_emp AS de
-ON ce.emp_no = de.emp_no
+    LEFT JOIN dept_emp AS de
+        ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
 SELECT * FROM retire_emp_by_dept;
+
+-- Salaries dates are not recent. Why?
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- List 1: Creating a list of retiring employees containing their 
+-- unique employee number, their last name, first name, gender and salary
+SELECT e.emp_no, e.first_name, 
+	e.last_name, e.gender,
+	s.salary, de.to_date
+INTO emp_info
+FROM employees AS e
+    INNER JOIN salaries AS s
+        ON (e.emp_no = s.emp_no)
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+    AND (de.to_date = '9999-01-01');
+
+-- List 2: a list of managers for each department, including 
+-- the department number, name, and the manager’s employee number, 
+-- last name, first name, and the starting and ending employment dates
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);	
+		
+SELECT * FROM manager_info;
+
+-- The question "Why are there only five active managers for nine departments?"
+-- is incorrect. There are active managers for all nine departments but only 
+-- 5 people from the list of retiring persons were/are the active managers of 
+-- departments at PH and only two of them are the active managers now. 
+-- So only two managers are going to retire soon and need to be replaced.
+
+-- List 3: An updated current_emp list that includes everything 
+-- it currently has, but also the employee’s departments
+-- (I want to see department number in this table too)
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	d.dept_name
+-- INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no);
+
+-- To answer the question why some employees appear twice in the List 3 we need 
+-- to narrow our search for only the Departments they are currently employed at 
+-- (because some of them might have switched the departments during their employment).
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	d.dept_name
+INTO dept_info
+FROM current_emp AS ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+WHERE de.to_date = '9999-01-01';
+
